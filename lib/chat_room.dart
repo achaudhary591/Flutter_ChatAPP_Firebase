292f4b1da97b72feb574ec3b947bb5461ed8be33
achaudhary591 <achaudhary591@gmail.com>
2023-01-26 15:16:25 +0530

    added one to one chat functionality

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoom extends StatelessWidget {
-
   final TextEditingController _message = TextEditingController();
   final Map<String, dynamic> userMap;
   final String? chatRoomId;
@@ -12,8 +11,8 @@ class ChatRoom extends StatelessWidget {
 
   ChatRoom({super.key, required this.userMap, required this.chatRoomId});
 
-  void onSendMessage() async{
-    if(_message.text.isNotEmpty){
+  void onSendMessage() async {
+    if (_message.text.isNotEmpty) {
       Map<String, dynamic> messages = {
         "sendby": _auth.currentUser?.displayName,
         "message": _message.text,
@@ -26,78 +25,103 @@ class ChatRoom extends StatelessWidget {
           .add(messages);
 
       _message.clear();
-    }else{
+    } else {
       print("Enter some text");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          userMap['name'],
+        appBar: AppBar(
+          title: Text(
+            userMap['name'],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: size.height / 1.3,
-              width: size.width,
-              child: StreamBuilder<QuerySnapshot>(
-                stream: _firestore.collection('chatroom')
-                    .doc(chatRoomId)
-                    .collection('chats')
-                    .orderBy("time", descending: false)
-                    .snapshots(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-                  if(snapshot.data != null){
-                    return ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context , index){
-                        return Text(snapshot.data!.docs[index]['message']);
-                      });
-                  }
-                  else{
-                    return Container();
-                  }
-                },
-              ),
-            ),
-            Container(
-              height: size.height / 10,
-              width: size.width,
-              alignment: Alignment.center,
-              child: SizedBox(
-                height: size.height / 12,
-                width: size.width / 1.1,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      height: size.height / 15,
-                      width: size.width / 1.28,
-                      child: TextField(
-                        controller: _message,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            )
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: onSendMessage,
-                    ),
-                  ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height / 1.3,
+                width: size.width,
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: _firestore
+                      .collection('chatroom')
+                      .doc(chatRoomId)
+                      .collection('chats')
+                      .orderBy("time", descending: false)
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.data != null) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            Map<String, dynamic>? map = snapshot.data?.docs[index].data() as Map<String, dynamic>?;
+                            return messages(size, map!);
+                          });
+                    } else {
+                      return Container();
+                    }
+                  },
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: size.height / 10,
+                width: size.width,
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: size.height / 12,
+                  width: size.width / 1.1,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: size.height / 15,
+                        width: size.width / 1.28,
+                        child: TextField(
+                          controller: _message,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          )),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.send),
+                        onPressed: onSendMessage,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget messages(Size size, Map<String, dynamic> map) {
+    return Container(
+      width: size.width,
+      alignment: map['sendby'] == _auth.currentUser?.displayName
+          ? Alignment.centerRight
+          : Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.blue
+        ),
+        child: Text(
+          map['message'],
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+
+          ),
         ),
       )
     );
