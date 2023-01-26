8ba97a922849e38396e859467356ed4ac5dae991
achaudhary591 <achaudhary591@gmail.com>
2023-01-26 16:00:15 +0530

    final commit

--- lib/Authenticate/Autheticate.dart
+++ lib/Authenticate/Autheticate.dart
@@ -1,19 +1,17 @@
-import 'package:chat_app_firebase/home_screen.dart';
-import 'package:chat_app_firebase/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Authenticate extends StatelessWidget {
-  // const Authenticate({Key? key}) : super(key: key);
+import '../Screens/HomeScreen.dart';
+import 'LoginScree.dart';
 
+class Authenticate extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
-    if(_auth.currentUser != null){
+    if (_auth.currentUser != null) {
       return HomeScreen();
-    }
-    else{
+    } else {
       return LoginScreen();
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'login_screen.dart';
+import 'Authenticate/Autheticate.dart';
 
-Future main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: Authenticate(),
       theme: ThemeData(
         useMaterial3: true,
       ),
     );
   }
 }
-
-
